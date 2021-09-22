973ca73e3fee8e213c5f0cffe12113cf0b8e91bf
Will Massey <82993915+wmassey776@users.noreply.github.com>
2021-09-22 07:43:53 -0400

    Update PROJECT.py
    Use a pseudocode to help construct the projects,

--- PROJECT.py
+++ PROJECT.py
@@ -1,1 +1,18 @@
-import random
+# We know we nneed the random module, so import it:
+from random import randint
+
+# Initialize the number of guesses (6)
+
+# Generate a random number (import random module)
+
+# Start while loop:
+
+  # Ask the user to guess number (use input functions)
+
+  # if the guess is just right then print "you win!" and break
+
+  # If the guess is too high then print "too high"
+  
+  # If the guess is too low then print  "too low"
+
+  # If player doesn't guess in the given number of turns - Game over, and break!
