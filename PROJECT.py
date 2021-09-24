b16bde2d085f55ec4f4ff39c11028108a788f702
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2021-09-24 14:02:53 -0400

    Update PROJECT.py

--- PROJECT.py
+++ PROJECT.py
@@ -1,29 +1,42 @@
-from random import randint
 
+def guess_the_number():
+    """This is called a docstring. Every function should have one.
+    It offers a summary description of the function the datatype value it returns.
+    Erase this and write a good docstring.
+    """
 
-def guess_number() -> None:
-    """
-    Player has 6 'turns' to guess a secret_number number between 1-20 correctly.
-    """
-    turns = 0
-    secret_number = randint(1, 20)
-    while user_input := int(input('Enter a number: ')):
-        turns += 1
-        if turns == 6:
-            print(f'You loose!, Secret number was {secret_number}')
+    # Generate a random number. We are creating a "memory location" of a random number.
+    # In order of rit to be static, we must create it before the while loop.
+    num_to_guess = randint(1, 20)
+    tries = 0
+    # Initialize the number of guesses (6)
+    while tries < 6:
+        tries = tries + 1
+        # When asking for user input, you need to store the user's guess
+        # in a memory location.Lets call it 'user_guess':
+        # And because we are dealing with integers, we will wrap user input in the int()
+        user_guess = int(input('HEY! Can you guess the number?'))
+        # If the guess is just right then print "you win!" and break
+        if user_guess == num_to_guess:
+            print('YOU WIN!')
+            print(num_to_guess)
             break
 
-        if user_input == secret_number:
-            print('You guessed correctly!')
-            break
+        # Before you have n < num_to_guess.
+        # Now, it's user_guess!
+        if user_guess < num_to_guess:
+            print('too low!\n')
+            print(num_to_guess)
 
-        elif user_input > secret_number:
-            print('Too high!')
-        elif user_input < secret_number:
-            print('Too low!')
-    print('Game over!')
+        # If the user_guess is too low then print  "too high"
+        if user_guess > num_to_guess:
+            print('too high!\n')
+            print(num_to_guess)
+        # If player doesn't guess in the given number of turns - print "Game over, " reveal number.
+        if tries == 6:
+            print('You lose')
+            break  # We need to break this loop! Because...
+        print('Game Over!')
 
-
-# # If the program is run (instead of imported), run the game
-if __name__ == "__main__":
-    print(guess_number())
+# We no longer need a parameter for this function
+guess_the_number()
