a83f858bc01373167abab07024115a3243247626
William <williamjaber94@gmail.com>
2021-09-24 15:09:48 -0500

    Update

--- PROJECT.py
+++ PROJECT.py
@@ -1,34 +1,28 @@
 from random import randint
 
-# Initialize the number of guesses (6)
-def guess_the_number(n):
+def guess_the_number():
+    """
+    The User will guess 'num_to_guess' number between 1-20 correctly
+    within a turn of 6 'tries' to win the game.
+    """
+    num_to_guess = randint(1, 20)
     tries = 0
-    while tries < 6:
-        tries = tries + 1
-# Generate a random number (import random module)
-        num_to_guess = randint(1, 20)
+    while user_input := int(input('HEY! Can you guess it? Enter a number: ')):
+        tries += 1
 
-# Ask the user to guess number (use input functions)
-        input('HEY! Can you guess the number?')
-        # for turn in str(tries):
-        #     print("try # " + turn)
-
-# If the guess is just right then print "you win!" and break
-        if n == num_to_guess:
+        if user_input == num_to_guess:
             print('YOU WIN!')
-            print(num_to_guess)
             break
 
-# If the guess is too high then print "too high"
-        if n < num_to_guess:
-                print('too low!\n')
-
-# If the guess is too low then print  "too low"
-        if n > num_to_guess:
-            print('too high!\n')
-
-# If player doesn't guess in the given number of turns - print "Game over, " reveal number.
         if tries == 6:
-            print('Game Over')
+            print(f'You failed, The number to guess was: {num_to_guess}')
+            break
 
-guess_the_number(5)
+        elif user_input > num_to_guess:
+            print('Too high!\n')
+            
+        elif user_input < num_to_guess:
+            print('Too low!\n')
+    print('Game Over!')
+
+guess_the_number()
